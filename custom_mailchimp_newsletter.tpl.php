29e4daa83a37b63024da58a397cafe4d3435387c
alexsergivan <alexsergivan@gmail.com>
2014-05-29 12:46:53 +0300

    Initial commit

---
+++ custom_mailchimp_newsletter.tpl.php
@@ -0,0 +1,15 @@
+<div style="padding: 50px;">
+  <div>
+    <div style="float: left; margin-right: 20px; margin-top: 10px;"><?php print $logo; ?></div> 
+    <div style="clear:both;"></div> 
+  </div>
+  <div>
+    <?php print $body; ?>	
+  </div> 
+  <div>
+    <?php print $main_content; ?>
+  </div> 	
+  <div>
+    <?php print $footer; ?>	
+  </div>
+</div>
